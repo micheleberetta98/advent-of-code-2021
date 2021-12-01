9e6a3e9cd9c627b6b8fa2215164930825280289a
micheleberetta98 <beretta.m98@gmail.com>
2021-12-01 21:57:22 +0100

    day 1 - part 2

--- 01/Main.hs
+++ 01/Main.hs
@@ -4,7 +4,7 @@ main :: IO ()
 main = do
   s <- readFile "input.txt"
   let nums = map read (lines s)
-  print (countIncreased nums)
+  print (countIncreased $ sum3by3 nums)
 
 
 countIncreased :: [Int] -> Int
@@ -12,3 +12,7 @@ countIncreased []     = 0
 countIncreased (x:xs) = length $ filter increased $ zip (x:xs) xs
   where
     increased (x, y) = x < y
+
+sum3by3 :: [Int] -> [Int]
+sum3by3 (x:y:z:xs) = (x + y + z) : sum3by3 (y:z:xs)
+sum3by3 _          = []
