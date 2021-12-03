1a3b0b5f760ae31d4efa76aad2d6ce329a5c4d88
micheleberetta98 <beretta.m98@gmail.com>
2021-12-03 10:34:09 +0100

    day 3 - part 2

--- 03/Main.hs
+++ 03/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
 
 import           Data.Char
@@ -9,20 +10,38 @@ main = do
   binarynums <- lines <$> readFile "input.txt"
   let (g, e) = rates binarynums
   print (toDec g  * toDec e)
+  let o = oxygenRating binarynums
+      c = co2rating binarynums
+  print (toDec o * toDec c)
 
 rates :: [String] -> (String, String)
 rates nums = (gamma, eps)
   where
     gamma = map mostCommon $ transpose nums
-    eps = map invert gamma
+    eps = map leastCommon $ transpose nums
 
-mostCommon :: [Char] -> Char
+oxygenRating :: [String] -> String
+oxygenRating = findRating mostCommon
+
+co2rating :: [String] -> String
+co2rating = findRating leastCommon
+
+mostCommon :: Ord a => [a] -> a
 mostCommon = head . maximumBy (compare `on` length) . group . sort
 
-invert :: Char -> Char
-invert '0' = '1'
-invert '1' = '0'
-invert x   = x -- For completeness
+leastCommon :: Ord a => [a] -> a
+leastCommon = head . minimumBy (compare `on` length) . group . sort
+
+findRating :: (String -> Char) -> [String] -> String
+findRating f = findRating' f . map (\x -> (x, x))
+  where
+    findRating' :: (String -> Char) -> [(String, String)] -> String
+    findRating' _ [b]  = fst b
+    findRating' f bits = findRating' f $ map (fmap tail) $ filter (\(x, y) -> head y == common) bits
+      where
+        numsToKeep = filter (\x -> head (snd x) == common) bits
+        common = f (map (head . snd) bits)
+
 
 toDec :: String -> Int
 toDec = foldl' (\acc x -> 2 * acc + digitToInt x) 0
