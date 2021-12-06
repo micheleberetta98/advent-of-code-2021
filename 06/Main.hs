39484add08da99c184d7cc9f4632e629a63abf06
micheleberetta98 <beretta.m98@gmail.com>
2021-12-06 09:27:53 +0100

    day 06 - part 2

--- 06/Main.hs
+++ 06/Main.hs
@@ -1,28 +1,37 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
-import           Lanternfish
+import qualified Data.Text as T
+
+type LanternfishCount = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
 
 main :: IO ()
 main = do
   fish <- parse <$> readFile "input.txt"
-  putStr "Answer 1:   " >> print (fishAtDay 80 (days fish))
+  putStr "Answer 1:   " >> print (fishAtDay 80 fish)
+  putStr "Answer 2:   " >> print (fishAtDay 256 fish)
 
 ------------ Functions
 
-fishAtDay :: Int -> [[Lanternfish]] -> Int
-fishAtDay n lst = length (lst !! n)
+fishAtDay :: Int -> LanternfishCount -> Int
+fishAtDay !n = count . apply n step
+  where
+    count (a, b, c, d, e, f, g, h, i) = a + b + c + d + e + f + g + h + i
+    step (a, b, c, d, e, f, g, h, i) = (b, c, d, e, f, g, h + a, i, a)
 
-days :: [Lanternfish] -> [[Lanternfish]]
-days = iterate (concatMap spawn)
+apply :: Int -> (a -> a) -> a -> a
+apply 0 f !x  = x
+apply !n f !x = apply (n-1) f (f x)
 
 ------------ Parsing
 
-parse :: String -> [Lanternfish]
-parse = map (Lanternfish . read) . splitOn ','
+parse :: String -> LanternfishCount
+parse = format . freqs . map read . splitOn ","
+  where
+    freqs :: [Int] -> [Int]
+    freqs l = map (\x -> length $ filter (== x) l) [1 .. 5]
+    format [a, b, c, d, e] = (0, a, b, c, d, e, 0, 0, 0) -- In the file there are only values from 1 to 5
 
-splitOn :: Char -> String -> [String]
-splitOn c s =
-  case dropWhile (== c) s of
-    "" -> []
-    s' -> t : splitOn c s''
-      where (t, s'') = break (== c) s'
+splitOn :: String -> String -> [String]
+splitOn sep s = T.unpack <$> T.splitOn (T.pack sep) (T.pack s)
