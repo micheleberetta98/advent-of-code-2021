037221c57aea14972134ff6efcc914e04115d64e
micheleberetta98 <31998227+micheleberetta98@users.noreply.github.com>
2021-12-07 09:00:59 +0100

    day 07 - part 2

--- 07/Main.hs
+++ 07/Main.hs
@@ -5,21 +5,26 @@ import           Data.List
 main :: IO ()
 main = do
   nums <- parse <$> readFile "input.txt"
-  putStr "Part 1:   " >> print (lowestFuel nums)
-  pure ()
+  putStr "Part 1:   " >> print (lowestFuel fuelCost nums)
+  putStr "Part 2:   " >> print (lowestFuel fuelCost' nums)
 
------------- Part 1
-
-lowestFuel :: [Int] -> Int
-lowestFuel nums = minimum $ map (fuelCost nums) minMaxInterval
+lowestFuel :: ([Int] -> Int -> Int) -> [Int] -> Int
+lowestFuel f nums = minimum $ map (f nums) minMaxInterval
   where
     (low, high) = minmax nums
     minMaxInterval = [low..high]
 
-fuelCost :: [Int] -> Int -> Int
-fuelCost xs m = sum $ map (\x -> abs (x - m)) xs
+------------ Functions
 
------------- Utils
+fuelCost :: [Int] -> Int -> Int
+fuelCost xs m = sum $ map (diff m) xs
+
+fuelCost' :: [Int] -> Int -> Int
+fuelCost' xs m = sum $ map (sumOfFirstN . diff m) xs
+  where sumOfFirstN n = (n * (n + 1)) `div` 2
+
+diff :: Num a => a -> a -> a
+diff x y = abs (x - y)
 
 minmax :: Ord a => [a] -> (a, a)
 minmax = (,) <$> minimum <*> maximum
