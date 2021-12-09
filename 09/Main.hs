cb7bd5378d28b6ae8a686c89fd442e94910d7b70
micheleberetta98 <beretta.m98@gmail.com>
2021-12-09 21:23:20 +0100

    day 09 - way faster now

--- 09/Main.hs
+++ 09/Main.hs
@@ -7,7 +7,7 @@ import           Data.Matrix
 
 main :: IO ()
 main = do
-  m <- parse <$> readFile "input.txt"
+  m <- fromLists . map (map digitToInt) . lines <$> readFile "input.txt"
   putStr "Answer 1:  " >> print (riskLevel m)
   putStr "Answer 2:  " >> print (largestBasins m)
 
@@ -18,22 +18,19 @@ riskLevel = sum . map (+1) . lowPoints
   where lowPoints m = map (m !) (lowIndices m)
 
 largestBasins :: Matrix Int -> Int
-largestBasins = product . take 3 . reverse . sort . map length . basins
-  where basins m = map (basin m) (lowIndices m)
+largestBasins m = product . take 3 . sortBy (flip compare) . map getBasinSize . lowIndices $ m
+  where getBasinSize ij = evalState (basinSize m ij) (False <$ m)
 
-basin :: Matrix Int -> (Int, Int) -> [(Int, Int)]
-basin m ij = evalState (basin' m ij) (False <$ m)
-
-basin' :: Matrix Int -> (Int, Int) -> State (Matrix Bool) [(Int, Int)]
-basin' m coords = do
+basinSize :: Matrix Int -> (Int, Int) -> State (Matrix Bool) Int
+basinSize m coords = do
   visited <- get
   if visited ! coords
-    then pure []
+    then pure 0
     else do
       let notVisited = filter (not . (visited !)) . filter isViable $ neighbouringIndices m coords
       modify' (setElem True coords)
-      rest <- concat <$> traverse (basin' m) notVisited
-      pure (coords : rest)
+      rest <- sum <$> traverse (basinSize m) notVisited
+      pure (1 + rest)
   where
     isViable ij = let k = m ! ij in k > m ! coords && k < 9
 
@@ -44,17 +41,9 @@ lowIndices = filter <$> isLow <*> indices
   where indices m = [(i, j) | i <- [1..nrows m], j <- [1..ncols m]]
 
 isLow :: Matrix Int -> (Int, Int) -> Bool
-isLow m pos = all (> x) (neighbours m pos)
-  where x = m ! pos
-
-neighbours :: Matrix a -> (Int, Int) -> [a]
-neighbours m = map (m !) . neighbouringIndices m
+isLow m pos = all (> m ! pos) neighbours
+  where neighbours = map (m !) (neighbouringIndices m pos)
 
 neighbouringIndices :: Matrix a -> (Int, Int) -> [(Int, Int)]
 neighbouringIndices m (i, j) = filter valid [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
   where valid (x, y) = x >= 1 && x <= nrows m && y >= 1 && y <= ncols m
-
------------- Utils
-
-parse :: String -> Matrix Int
-parse = fromLists . map (map digitToInt) . lines
