4edf1002c7d3d9f94c4ed334c618942cc7810f0e
micheleberetta98 <beretta.m98@gmail.com>
2021-12-09 14:43:49 +0100

    day 09 - part 2

--- 09/Main.hs
+++ 09/Main.hs
@@ -1,26 +1,43 @@
 module Main where
 
 import           Data.Char
-import           Data.Maybe
+import           Data.List
 import           Matrix
 
 main :: IO ()
 main = do
   m <- parse <$> readFile "input.txt"
   putStr "Answer 1:  " >> print (riskLevel m)
+  putStr "Answer 2:  " >> print (largestBasins m)
+
+------------ Solutions
 
 riskLevel :: Matrix Int -> Int
-riskLevel = sum . map (+1) . catMaybes . lowPoints
+riskLevel = sum . map (+1) . lowPoints
+  where lowPoints m = map (m `at`) (lowIndices m)
 
-lowPoints :: Matrix Int -> [Maybe Int]
-lowPoints m = map (m `at`) $ filter (isLow m) indices
+largestBasins :: Matrix Int -> Int
+largestBasins = product . take 3 . reverse . sort . map length . basins
+  where basins m = map (basin (False <$ m) m) (lowIndices m)
+
+basin :: Matrix Bool -> Matrix Int -> (Int, Int) -> [(Int, Int)]
+basin visited m currentCoords = nub $ currentCoords : concatMap (basin visited' m) notVisited
   where
-    (rows, cols) = dimensions m
-    indices = [(i, j) | i <- [0..rows-1], j <- [0..cols-1]]
+    visited' = set currentCoords True visited
+    notVisited = filter isViable $ neighbouringIndices m currentCoords
+    isViable ij =
+      let k = m `at` ij
+          x = m `at` currentCoords
+      in not (visited `at` ij) && k > x && k < 9
+
+------------ Utils
+
+lowIndices :: Matrix Int -> [(Int, Int)]
+lowIndices = filter <$> isLow <*> indices
 
 isLow :: Matrix Int -> (Int, Int) -> Bool
 isLow m pos = all (> x) (neighbours m pos)
-  where Just x = m `at` pos
+  where x = m `at` pos
 
 parse :: String -> Matrix Int
 parse = fromList . map (map digitToInt) . lines
--- 09/Matrix.hs
+++ 09/Matrix.hs
@@ -1,10 +1,10 @@
 module Matrix where
 
-import           Data.Maybe
-import           Data.Vector (Vector, (!?))
+import           Data.Vector (Vector, (!), (!?), (//))
 import qualified Data.Vector as V
 
 data Matrix a = Matrix (Int, Int) (Vector (Vector a))
+  deriving (Show)
 
 fromList :: [[a]] -> Matrix a
 fromList lst = Matrix dims . V.fromList . map V.fromList $ lst
@@ -13,14 +13,23 @@ fromList lst = Matrix dims . V.fromList . map V.fromList $ lst
 dimensions :: Matrix a -> (Int, Int)
 dimensions (Matrix d _) = d
 
-at :: Matrix a -> (Int, Int) -> Maybe a
-(Matrix _ m) `at` (i, j) = m !? i >>= (!? j)
+at :: Matrix a -> (Int, Int) -> a
+(Matrix _ m) `at` (i, j) = m ! i ! j
+
+set :: (Int, Int) -> a -> Matrix a -> Matrix a
+set (i, j) value (Matrix dims m) = Matrix dims $ m // [(i, updatedRow (m ! i))]
+  where updatedRow v = v // [(j, value)]
 
 neighbours :: Matrix a -> (Int, Int) -> [a]
-neighbours m (i, j) = catMaybes neighbours'
-  where neighbours' =
-          [ m `at` (i-1, j)
-          , m `at` (i+1, j)
-          , m `at` (i, j-1)
-          , m `at` (i, j+1)
-          ]
+neighbours m = map (m `at`) . neighbouringIndices m
+
+neighbouringIndices :: Matrix a -> (Int, Int) -> [(Int, Int)]
+neighbouringIndices (Matrix (rows, cols) _) (i, j) = filter valid [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+  where valid (x, y) = x >= 0 && x < rows && y >= 0 && y < cols
+
+indices :: Matrix a -> [(Int, Int)]
+indices m = [(i, j) | i <- [0..rows-1], j <- [0..cols-1]]
+  where (rows, cols) = dimensions m
+
+instance Functor Matrix where
+  fmap f (Matrix dims m) = Matrix dims $ fmap (fmap f) m
