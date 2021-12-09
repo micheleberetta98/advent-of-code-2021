f47bcd581efa98af23997bf1e165d0281d9d6280
micheleberetta98 <beretta.m98@gmail.com>
2021-12-09 20:41:41 +0100

    day 09 - changed package, minor adjustments

--- 09/Main.hs
+++ 09/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           Data.Char
 import           Data.List
-import           Matrix
+import           Data.Matrix
 
 main :: IO ()
 main = do
@@ -14,7 +14,7 @@ main = do
 
 riskLevel :: Matrix Int -> Int
 riskLevel = sum . map (+1) . lowPoints
-  where lowPoints m = map (m `at`) (lowIndices m)
+  where lowPoints m = map (m !) (lowIndices m)
 
 largestBasins :: Matrix Int -> Int
 largestBasins = product . take 3 . reverse . sort . map length . basins
@@ -23,21 +23,33 @@ largestBasins = product . take 3 . reverse . sort . map length . basins
 basin :: Matrix Bool -> Matrix Int -> (Int, Int) -> [(Int, Int)]
 basin visited m currentCoords = nub $ currentCoords : concatMap (basin visited' m) notVisited
   where
-    visited' = set currentCoords True visited
+    visited' = setElem True currentCoords visited
     notVisited = filter isViable $ neighbouringIndices m currentCoords
     isViable ij =
-      let k = m `at` ij
-          x = m `at` currentCoords
-      in not (visited `at` ij) && k > x && k < 9
+      let k = m ! ij
+          x = m ! currentCoords
+      in not (visited ! ij) && k > x && k < 9
 
 ------------ Utils
 
 lowIndices :: Matrix Int -> [(Int, Int)]
 lowIndices = filter <$> isLow <*> indices
+  where indices m = [(i, j) | i <- [1..nrows m], j <- [1..ncols m]]
 
 isLow :: Matrix Int -> (Int, Int) -> Bool
 isLow m pos = all (> x) (neighbours m pos)
-  where x = m `at` pos
+  where x = m ! pos
+
+neighbours :: Matrix a -> (Int, Int) -> [a]
+neighbours m = map (m !) . neighbouringIndices m
+
+neighbouringIndices :: Matrix a -> (Int, Int) -> [(Int, Int)]
+neighbouringIndices m (i, j) = filter valid [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+  where
+    (rows, cols) = (nrows m, ncols m)
+    valid (x, y) = x >= 1 && x <= rows && y >= 1 && y <= cols
+
+------------ Utils
 
 parse :: String -> Matrix Int
-parse = fromList . map (map digitToInt) . lines
+parse = fromLists . map (map digitToInt) . lines
