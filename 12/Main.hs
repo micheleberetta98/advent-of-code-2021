55dc9f682d6112411c9ed88bb73d3cacd62b0114
micheleberetta98 <beretta.m98@gmail.com>
2021-12-12 13:16:45 +0100

    day 12 - optimization

--- 12/Main.hs
+++ 12/Main.hs
@@ -2,20 +2,24 @@ module Main where
 
 import           CaveSystem
 import           Data.List
+import           Data.Map   (Map, (!?))
+import qualified Data.Map   as M
+import           Data.Maybe
 
 main :: IO ()
 main = do
   system <- buildSystem . parse <$> readFile "input.txt"
-  putStr "Answer 1: " >> print (length $ allPathsWith (elem, visit1)       system)
+  putStr "Answer 1: " >> print (length $ allPathsWith (M.member, visit1)       system)
   putStr "Answer 2: " >> print (length $ allPathsWith (isVisited2, visit2) system)
 
 ------------ Solutions
 
-type IsVisitedFn = Cave -> [Cave] -> Bool
-type VisitFn = Cave -> [Cave] -> [Cave]
+type Seen = Map Cave Int
+type IsVisitedFn = Cave -> Seen -> Bool
+type VisitFn = Cave -> Seen -> Seen
 
 allPathsWith :: (IsVisitedFn, VisitFn) -> CaveSystem -> [[Cave]]
-allPathsWith (isVisited, visit) system = allPaths' [] (startCave system)
+allPathsWith (isVisited, visit) system = allPaths' M.empty (startCave system)
   where
     allPaths' _ c@(EndCave _ _) = [[c]]
     allPaths' seen cave
@@ -24,24 +28,24 @@ allPathsWith (isVisited, visit) system = allPaths' [] (startCave system)
 
 visit1 :: VisitFn
 visit1 (BigCave _ _) s = s
-visit1 cave          s = if cave `elem` s then s else cave : s
+visit1 cave          s = M.insertWith (+) cave 1 s
 
 isVisited2 :: IsVisitedFn
-isVisited2 cave@(BigCave _ _) seen        = False
-isVisited2 cave@(SmallCave _ _) seen = not (null visits) && doubleVisitedSmallCave seen
-  where visits = filter (== cave) seen
-isVisited2 cave seen                 = cave `elem` seen
+isVisited2 cave@(BigCave _ _) _ = False
+isVisited2 cave@(SmallCave _ _) seen = visits == 2 || (visits == 1 && doubleVisitedSmallCave seen)
+  where visits = fromMaybe 0 (seen !? cave)
+isVisited2 cave seen = cave `M.member` seen
 
 visit2 :: VisitFn
 visit2 cave@(SmallCave _ _) seen
-  | null visits                 = cave : seen
+  | visits == 0                 = M.insertWith (+) cave 1 seen
   | doubleVisitedSmallCave seen = seen
-  | otherwise                   = cave : seen
-  where visits = filter (== cave) seen
+  | otherwise                   = M.insertWith (+) cave 1 seen
+  where visits = fromMaybe 0 (seen !? cave)
 visit2 c s = visit1 c s
 
-doubleVisitedSmallCave :: [Cave] -> Bool
-doubleVisitedSmallCave = any ((>1) . length) . group . sort . filter isSmall
+doubleVisitedSmallCave :: Map Cave Int -> Bool
+doubleVisitedSmallCave =  any isSmall . M.keys . M.filter (> 1)
 
 ------------ Parsing
 
